324abaf29ffb45a178d3aff31801a4fc97094172
Steven Kandow <kando1sr@gmail.com>
2020-03-30 16:24:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,8 +5,8 @@ second_number = 12
 
 sum = first_number + second_number 
 
-difference = first_number-second_number
+difference = first_number - second_number
 
-product = first_number*second_number
+product = first_number * second_number
 
-difference = first_number/second_number
+difference = first_number / second_number
